814a9100380adc446d9ba4109d350dd5740bf03a
Thainá Pires <thainaspiress@gmail.com>
2024-10-08 21:10:12 -0300

    Create livewire components

---
+++ resources/views/livewire/projects/proposals.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Componete livewire project proposals
+</div>
---
+++ resources/views/livewire/projects/show.blade.php
@@ -0,0 +1,8 @@
+<div>
+    Componente Livewire projects.show
+
+    <pre>
+        title: {{$project->title}}
+        description: {!! $project->description !!}
+    </pre>
+</div>
