683f3960b75bfbd87798f971fca7e9ff54864b7d
Thainá Pires <thainaspiress@gmail.com>
2024-10-10 21:33:10 -0300

    Add command that update status of a project

--- app/Notifications/DecreasedPosition.php
+++ app/Notifications/DecreasedPosition.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class DecreasedPosition extends Notification
+class DecreasedPosition extends Notification implements ShouldQueue
 {
     use Queueable;
 
